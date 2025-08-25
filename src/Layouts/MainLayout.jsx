ef43d2c65b822d1a6099542d818637485722195b
omnia-hamdy538 <hamdymenna538@gmail.com>
2025-08-25 15:12:00 +0300

    Fixed Navbar import path

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Outlet} from "react-router-dom"
-import Navbar from '../components/Navbar'
+
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 export default function MainLayout() {
   return (
     <div>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,59 +1,71 @@
-import { Button } from '@heroui/react'
-import PostActions from '../Post/PostActions'
-// import React, { useEffect, useRef, useState } from 'react'
+// import { Button } from '@heroui/react'
+// import PostActions from '../Post/PostActions'
+// // import React, { useEffect, useRef, useState } from 'react'
 
-export default function ProfilePage() {
-  // const [count1, setCount1] = useState(0)
-  // const [count2, setCount2] = useState(0)
-  // // const [numOfChanges, setNumOfChanges] = useState(-1);
-  // const numOfChanges = useRef(-1)
+// export default function ProfilePage() {
+//   // const [count1, setCount1] = useState(0)
+//   // const [count2, setCount2] = useState(0)
+//   // // const [numOfChanges, setNumOfChanges] = useState(-1);
+//   // const numOfChanges = useRef(-1)
 
 
 
-  // // useEffect(()=>{
-  // //   setNumOfChanges(numOfChanges+1);
-  // // },[count1,count2])
+//   // // useEffect(()=>{
+//   // //   setNumOfChanges(numOfChanges+1);
+//   // // },[count1,count2])
 
-  // useEffect(()=>{
-  //     numOfChanges.current++;
-  // })
+//   // useEffect(()=>{
+//   //     numOfChanges.current++;
+//   // })
 
     
 
-  return (
+//   return (
 
 
-    <div>
-          <PostActions id="lksmfosm"/>
-          <PostActions id="lksmfosm"/>
+//     <div>
+//           <PostActions id="lksmfosm"/>
+//           <PostActions id="lksmfosm"/>
 
 
 
 
-    </div>
-    //      <div className='grid gap-3 max-w-2xl mx-auto'>
-    //      <div className='grid grid-cols-3 text-center'>
-    //        <div>
-    //          <h1>{count1}</h1>
-    //           <div className="flex justify-center gap-2 mt-3">
-    //               <Button color='success' onPress={()=>setCount1(count1+1)}>increase</Button>
-    //               <Button color='warning' onPress={()=>setCount1(count1-1)}>decrease</Button>
-    //           </div>
-    //        </div>
-    //        <div>
-    //           <h1>Num of changes:</h1>
-    //           <h1>{numOfChanges.current ==-1 ? 0 : numOfChanges.current}</h1>
-    //        </div>
-    //         <div>
-    //          <h1>{count2}</h1>
-    //           <div className="flex justify-center gap-2 mt-3">
-    //               <Button color='success' onPress={()=>setCount2(count2+1)}>increase</Button>
-    //               <Button color='warning' onPress={()=>setCount2(count2-1)}>decrease</Button>
-    //           </div>
-    //         </div>
-    //      </div>
+//     </div>
+//     //      <div className='grid gap-3 max-w-2xl mx-auto'>
+//     //      <div className='grid grid-cols-3 text-center'>
+//     //        <div>
+//     //          <h1>{count1}</h1>
+//     //           <div className="flex justify-center gap-2 mt-3">
+//     //               <Button color='success' onPress={()=>setCount1(count1+1)}>increase</Button>
+//     //               <Button color='warning' onPress={()=>setCount1(count1-1)}>decrease</Button>
+//     //           </div>
+//     //        </div>
+//     //        <div>
+//     //           <h1>Num of changes:</h1>
+//     //           <h1>{numOfChanges.current ==-1 ? 0 : numOfChanges.current}</h1>
+//     //        </div>
+//     //         <div>
+//     //          <h1>{count2}</h1>
+//     //           <div className="flex justify-center gap-2 mt-3">
+//     //               <Button color='success' onPress={()=>setCount2(count2+1)}>increase</Button>
+//     //               <Button color='warning' onPress={()=>setCount2(count2-1)}>decrease</Button>
+//     //           </div>
+//     //         </div>
+//     //      </div>
  
-    //  </div>
+//     //  </div>
 
+//   )
+// }
+
+
+import React from 'react'
+
+export default function ProfilePage() {
+  return (
+    <div>
+      Profile Page
+    </div>
   )
 }
+

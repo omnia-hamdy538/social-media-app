bc6a4d48599f20c721a4aa31c8ce9b31d2012c1c
omnia-hamdy538 <hamdymenna538@gmail.com>
2025-08-25 16:10:25 +0300

    Fixed Navbar import path

--- src/Post/PostBody.jsx
+++ src/Post/PostBody.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PostBody({caption,image}) {
   return (
     <div>
-    {caption && <p>{caption}</p>}
+    {caption && <p className='break-all'>{caption}</p>}
     {image && <img src={image} className='w-full h-100 object-cover mt-2' alt="" />}
 
     </div>
